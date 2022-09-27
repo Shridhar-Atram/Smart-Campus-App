b5eaa01219c7a17e110ab83427d9ce12afde264d
shridhar atram <shridharatram555@gmail.com>
2022-09-27 16:22:31 +0530

    added some dependencies

--- lib/screens/help.dart
+++ lib/screens/help.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
@@ -7,7 +7,7 @@ class help extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("Dashboard Page"),
+        child: Text("setting Page"),
       ),
     );
   }
