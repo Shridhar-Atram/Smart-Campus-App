4e0810dd4cded0a6fa8e903ab200ba9dfcb70b84
Shrutika Sonawane <shrusonawane811@gmali.com>
2022-10-31 20:34:53 +0530

    Forum added

--- lib/screens/help.dart
+++ lib/screens/help.dart
@@ -2,19 +2,52 @@
 
 import 'package:flutter/material.dart';
 import './lostandfound.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'forum.dart';
 
 class help extends StatelessWidget {
+ // final User? user = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: ElevatedButton(
-            child: Text('Lost And Found'),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => LostAndFound()));
-            }),
+    return Scaffold(
+      appBar: AppBar(title: Text('Help')),
+      body: Center(
+      child: 
+      Container(
+        margin:EdgeInsets.only(top:300),
+        alignment:Alignment.center,
+        child:
+        Column(
+        children: [
+          Container(
+            width:190,
+            height:60,
+             padding:EdgeInsets.fromLTRB(4, 5, 4, 5),
+            child:
+           ElevatedButton(
+              child: Text('Forum',style:TextStyle(fontSize:20)),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Chat()));
+              }),
+          ),
+          SizedBox(height:20),
+         Container(
+           padding:EdgeInsets.fromLTRB(4, 5, 4, 5),
+              width:200,
+            height:60,
+          child: ElevatedButton(
+              child: Text('Lost And Found',style:TextStyle(fontSize:20)),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => LostAndFound()));
+              }),)
+         
+        ],
       ),
+    ))
     );
+   
   }
 }
